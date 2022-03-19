63525020a0222aa288454927580cd7f42390ec6b
david <davidof1977@hotmail.com>
2022-03-19 22:38:55 +0100

    Operators y comparators

--- src/main/java/es/agroseguro/functional/BuilitFunctionalInterfaces.java
+++ src/main/java/es/agroseguro/functional/BuilitFunctionalInterfaces.java
@@ -1,9 +1,15 @@
 package es.agroseguro.functional;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
 
 public class BuilitFunctionalInterfaces {
 
@@ -30,6 +36,38 @@ public class BuilitFunctionalInterfaces {
 		
 		Integer numero =  transform.andThen(multiplicarX2).apply("Hola", "Mundo");
 		System.out.println(numero);	
+		
+		//UnaryOpertor
+		UnaryOperator<Integer> operador = n -> n++;
+		
+		//BinaryOperator
+		BinaryOperator<Integer> biOperator = (n,m) -> n + m;
+		
+		//Comparators
+		Comparator<Integer> comp1 = (n, m) ->  n - m;
+		
+		List<Integer> lista = Arrays.asList(1,4,2,9,34,5);
+		
+		lista.sort(comp1);
+		
+		System.out.println(lista);
+		
+		List<Animal> animals = new ArrayList<Animal>();
+		animals.add(new Animal("fish", false, true));
+		animals.add(new Animal("kangaroo", true, false));
+		animals.add(new Animal("rabbit", true, false));
+		animals.add(new Animal("turtle", false, true));
+		animals.add(new Animal("tiger", false, true));
+		
+		Comparator<Animal> compAlfabetico = (n, m) -> n.toString().compareTo(m.toString());
+		Comparator<Animal> compSize = (n, m) -> n.toString().length() - m.toString().length();
+		
+		animals.sort(compAlfabetico.thenComparing(compSize));
+		System.out.println(animals);
+		
+		animals.sort(compAlfabetico.thenComparing(compSize).reversed());
+		System.out.println(animals);
+		
 	}
 
 }

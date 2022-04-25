8ca35ff9c3ff754de245b58f4e7190315cd57ef1
Baralo <Baralo@AGRVDI35.agroseguro.com>
2022-04-25 11:21:42 +0200

    Borrar resultados ejercicios

--- src/main/java/es/agroseguro/functional/sesion1/Ejercicio1.java
+++ src/main/java/es/agroseguro/functional/sesion1/Ejercicio1.java
@@ -20,32 +20,16 @@ public class Ejercicio1 {
 		parcela.add(new Parcela(3,2,"a"));
 		parcela.add(new Parcela(3,4,""));
 		//Ordenar las parcelas en orden alfabetico por nombre.
-		parcela.sort((p1,p2)->p1.getNombre().compareTo(p2.getNombre()));
 		
 		//Obtener todas aquellas cuyo numero de parcela sea par
-		List<Parcela> parcelasPares = new ArrayList<Parcela>();
-		Predicate<Parcela> pares = p -> p.getNumero() % 2 == 0;
-		for (Parcela p3 : parcela) {
-			if (pares.test(p3)) {
-				parcelasPares.add(p3);
-			}
-		}
+
 		//Imprimir los nombres de las parcelas resultantes usando un consumer
-		Consumer<Parcela> consumer = p -> System.out.println(p);;
-		for (Parcela parcela2 : parcelasPares) {
-			consumer.accept(parcela2);
-		}
 		
 		//Hacer una funcion que reciba una parcela y devuelva la suma de su hoja y parcela.
-		Function<Parcela, Integer> func1 = p -> p.getNumero() + p.getNumero();
 		
 		//Hacer una funcion que reciba dos parcelas y devuelva aquella cuyo numero de hoja sea mayor
-		BinaryOperator<Parcela> operator = (p1,p2) -> p1.getHoja()>p2.getHoja()?p1:p2;
 		
 		//Todo junto
-		System.out.println("Usando streams");
-		parcela.stream().sorted((p1,p2)->p1.getNombre().compareTo(p2.getNombre())).
-			filter(pares).forEach(System.out::println);
 	}
 
 }
--- src/main/java/es/agroseguro/functional/sesion2/streams/Ejercicio2.java
+++ src/main/java/es/agroseguro/functional/sesion2/streams/Ejercicio2.java
@@ -14,36 +14,17 @@ public class Ejercicio2 {
 		List<Parcela> parcelas = GeneradorBeans.generarParcelas(3,5);
 		
 		//Imprimir la lista de parcelas
-		parcelas.forEach(p -> {
-			System.out.println(p);
-//			p.getTiposCapital().forEach(System.out::println);
-		});
-		System.out.println("------------------------------");
+
 		//Obtener las parcelas cuyos numero de parcela sean pares y pintar sus nombres
-		List<Parcela> parcelas2 = parcelas.stream().filter(p-> p.getNumero() % 2 == 0).collect(Collectors.toList());
-		parcelas2.forEach(p -> System.out.println(p.getNombre()));
-		System.out.println("------------------------------");
+
 		//Obtener una nueva lista de parcelas en las que todos los tipos de capital tenga valor mayor que 5000
-		parcelas2 = parcelas.stream().filter(p-> p.getTiposCapital().stream().allMatch(tc-> tc.getProduccion()>5000)).collect(Collectors.toList());
+
 		//Comprobar que lo anterior es correcto
-		parcelas2.stream().flatMap(p->p.getTiposCapital().stream()).forEach(System.out::println);
-		System.out.println("------------------------------");
+
 		//Obtener el valor de produccion de todas las parcelas
-		int suma = parcelas.stream().flatMap(p -> p.getTiposCapital().stream()).map(tc-> tc.getProduccion()).reduce(0,(p1,p2)-> p1 + p2);
-		System.out.println("Valor produccion= " + suma);
-		System.out.println("------------------------------");
+
 		//Obtener la parcela cuyo valor de produccion sea el mas alto
-		List<Parcela> aux = parcelas.stream().sorted((p1,p2) -> compareParcelas(p1, p2)).collect(Collectors.toList());
-		Optional<Parcela> maxP = aux.stream().findFirst();
 		
-		System.out.println(maxP);
 	}
-	
-	private static int compareParcelas(Parcela p1, Parcela p2) {
-		Optional<TipoCapital> opt1 =  p1.getTiposCapital().stream().max((t1,t2) -> t1.getProduccion() - t2.getProduccion());
-		Optional<TipoCapital> opt2 =  p2.getTiposCapital().stream().max((t1,t2) -> t1.getProduccion() - t2.getProduccion());
-		return opt2.get().getProduccion()-opt1.get().getProduccion();
-	}
-	
 	
 }

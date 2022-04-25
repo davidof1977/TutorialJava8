abf51c20ac95b78a0b591f05ef140e9a6ecb9247
Baralo <Baralo@AGRVDI35.agroseguro.com>
2022-04-25 11:24:37 +0200

    Borrar historial ejercicios

---
+++ src/main/java/es/agroseguro/functional/sesion2/streams/CopiaEjercicio2.java
@@ -0,0 +1,30 @@
+package es.agroseguro.functional.sesion2.streams;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import es.agroseguro.functional.beans.Parcela;
+import es.agroseguro.functional.beans.TipoCapital;
+import es.agroseguro.functional.utils.GeneradorBeans;
+
+public class CopiaEjercicio2 {
+
+	public static void main(String[] args) {
+		List<Parcela> parcelas = GeneradorBeans.generarParcelas(3,5);
+		
+		//Imprimir la lista de parcelas
+
+		//Obtener las parcelas cuyos numero de parcela sean pares y pintar sus nombres
+
+		//Obtener una nueva lista de parcelas en las que todos los tipos de capital tenga valor mayor que 5000
+
+		//Comprobar que lo anterior es correcto
+
+		//Obtener el valor de produccion de todas las parcelas
+
+		//Obtener la parcela cuyo valor de produccion sea el mas alto
+		
+	}
+	
+}

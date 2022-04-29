ed37f30f0d0e160d9e0be772b0227685ea9d982e
Baralo <Baralo@AGRVDI35.agroseguro.com>
2022-04-29 08:43:08 +0200

    Cambios sesion1

--- src/main/java/es/agroseguro/functional/sesion1/LambdasSearch.java
+++ src/main/java/es/agroseguro/functional/sesion1/LambdasSearch.java
@@ -10,31 +10,26 @@ public class LambdasSearch {
 	public static void main(String[] args) {
 
 		// list of animals
-				List<Animal> animals = new ArrayList<Animal>();
-				animals.add(new Animal("fish", false, true));
-				animals.add(new Animal("kangaroo", true, false));
-				animals.add(new Animal("rabbit", true, false));
-				animals.add(new Animal("turtle", false, true));
+		List<Animal> animals = new ArrayList<Animal>();
+		animals.add(new Animal("fish", false, true));
+		animals.add(new Animal("kangaroo", true, false));
+		animals.add(new Animal("rabbit", true, false));
+		animals.add(new Animal("turtle", false, true));
 
 		// pass class that does check
-				print(animals, a -> a.puedeSaltar());
-				
-				//print(animals, a -> a.puedeNadar());
-				
-				animals.stream().filter(animal -> animal.puedeSaltar()).peek(System.out::println).forEach(a->System.out.print(""));
-				Stream<Animal> s1 = animals.stream();
-				s1.filter(animal -> animal.puedeSaltar()).peek(System.out::println).forEach(a->System.out.print(""));
-				s1.filter(animal -> animal.puedeSaltar()).peek(System.out::println).forEach(a->System.out.print(""));
-				
-			}
+		print(animals, a -> a.puedeSaltar());
 
-			private static void print(List<Animal> animals, comprobarHabilidad checker) {
-				for (Animal animal : animals) {
+		print(animals, a -> a.puedeNadar());
 
-		// the general check
-					if (checker.test(animal))
-						System.out.print(animal + " ");
-				}
-				System.out.println();
-			}
+	}
+
+	private static void print(List<Animal> animals, comprobarHabilidad checker) {
+		for (Animal animal : animals) {
+
+			// the general check
+			if (checker.test(animal))
+				System.out.print(animal + " ");
+		}
+		System.out.println();
+	}
 }
